4bb9e5b4bc73991f463b4d46953262019d77da4b
Alireza Sadraii Rad <sadraiiali@gmail.com>
2024-02-03 23:00:30 +0100

    add static ip address

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,10 @@
 export LD_LIBRARY_PATH=/app/bin/lib/:$LD_LIBRARY_PATH
 
 
-cd /app/bin && /app/bin/start.sh -t $TEAM_NAME &
+cd /app/bin && /app/bin/start.sh \
+    -t $TEAM_NAME \
+    --host $HOST \
+    --port $PORT &
 
 cd /app/code && python3 /app/code/grpc-server.py
 
