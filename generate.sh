4cb4b689c35f4bf41f35220c135add60d1cde62d
arefsa78 <arefsayareh@gmail.com>
2024-04-22 23:24:15 -0230

    update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,2 +1,1 @@
-python3 -m grpc_tools.protoc -I./protos --python_out=./sample-low-level-team --pyi_out=./sample-low-level-team --grpc_python_out=./sample-low-level-team ./protos/*.proto
-python3 -m grpc_tools.protoc -I./protos --python_out=./sample-high-level-team --pyi_out=./sample-high-level-team --grpc_python_out=./sample-high-level-team ./protos/*.proto
+python3 -m grpc_tools.protoc -I./protos --python_out=./ --pyi_out=./ --grpc_python_out=./ ./protos/*.proto
